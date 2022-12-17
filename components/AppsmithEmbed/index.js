312737ddee1bd57545098850a6dc0b211e5c6e13
devMuangi <devmuangi@gmail.com>
2022-12-17 09:34:33 +0000

    init

---
+++ components/AppsmithEmbed/index.js
@@ -0,0 +1,10 @@
+function AppsmithEmbed() {
+    return (
+      <div className="App">
+        <h3>Gutheria management area: </h3>
+        <iframe src="https://app.appsmith.com/app/gutheria/homepage-639870fc038c74195c0ab098?branch=main"></iframe>
+      </div>
+    );
+  }
+  
+  export default AppsmithEmbed;
